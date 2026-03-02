146a1b17fe0eab12c20c4cfe28699350b1b5bd2d
Андрей Боев <a.boev.it@gmail.com>
2026-03-02 13:44:53 +0300

    NEW: Двумерные списки + Проверка ДЗ

--- 4. Циклы/4.2 Цикл while/Задания/Исполнитель раздвоитель.py
+++ 4. Циклы/4.2 Цикл while/Задания/Исполнитель раздвоитель.py
@@ -1,20 +1,9 @@
 a = int(input())
 b = int(input())
-while a != b:
-    if a % 2 == 0:
-        if a < b:
-            a = a * 2
-            a = a - 1
-        if a == b:
-            break
-        a = a / 2
-        if a >= b:
-            print(":2")
-        if a - 1 == b or a - 2 == b:
-            a -= 1
-            print("-1")
+while a > b:
+    if a % 2 == 0 and a // 2 >= b:
+        print(':2')
+        a //= 2
     else:
-        if a < b:
-            a = a * 2
+        print('-1')
         a -= 1
-        print("-1")
