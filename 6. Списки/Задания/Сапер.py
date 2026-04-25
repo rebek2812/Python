fa38e4fb777836f548ff92f2f4b30d0a7c58051e
Андрей Боев <a.boev.it@gmail.com>
2026-04-25 12:27:28 +0300

    UPD: Исправление

--- 6. Списки/Задания/Сапер.py
+++ 6. Списки/Задания/Сапер.py
@@ -1,29 +1,27 @@
-N,M,K = list(map(int, input().split()))
+N, M, K = list(map(int, input().split()))
 matrix = [[0 for j in range(M)] for i in range(N)]
 while K != 0:
-    a,b = list(map(int, input().split()))
+    a, b = list(map(int, input().split()))
     a -= 1
     b -= 1
     matrix[a][b] = "*"
-    if a - 1 >= 0 and b - 1>= 0 and matrix[a - 1][b- 1] != "*" :
-        matrix[a - 1][b- 1] += 1
-    if a - 1 >= 0 and b>= 0 and matrix[a - 1][b] != "*" :
+    if a - 1 >= 0 and b - 1 >= 0 and matrix[a - 1][b - 1] != "*":
+        matrix[a - 1][b - 1] += 1
+    if a - 1 >= 0 and matrix[a - 1][b] != "*":
         matrix[a - 1][b] += 1
-    if a - 1>= 0 and b + 1>= 0 and b + 1 < M and matrix[a - 1][b+1] != "*":
-        matrix[a - 1][b+1] += 1
-    if a >= 0 and b - 1 >= 0 and b-1 < M and matrix[a][b-1] != "*":
+    if a - 1 >= 0 and b + 1 < M and matrix[a - 1][b + 1] != "*":
+        matrix[a - 1][b + 1] += 1
+    if b - 1 >= 0 and matrix[a][b - 1] != "*":
         matrix[a][b - 1] += 1
-    if a >= 0 and b + 1 >= 0 and b+1 < M and matrix[a][b+1] != "*":
+    if b + 1 < M and matrix[a][b + 1] != "*":
         matrix[a][b + 1] += 1
-    if a+1 >= 0 and b - 1 >= 0 and b-1 < M and a+1<N and  matrix[a+1][b-1] != "*":
-        matrix[a+1][b - 1] += 1
-    if a + 1 >= 0 and b >= 0 and b < M and a + 1 < N and matrix[a + 1][b] != "*":
+    if b - 1 >= 0 and a + 1 < N and matrix[a + 1][b - 1] != "*":
+        matrix[a + 1][b - 1] += 1
+    if a + 1 < N and matrix[a + 1][b] != "*":
         matrix[a + 1][b] += 1
-    if a+1 >= 0 and b + 1 >= 0 and b+1 < M and a+1<N and  matrix[a+1][b+1] != "*":
-        matrix[a+1][b + 1] += 1
-
-
-    K-=1
+    if b + 1 < M and a + 1 < N and matrix[a + 1][b + 1] != "*":
+        matrix[a + 1][b + 1] += 1
+    K -= 1
 for i in range(N):
     for j in range(M):
         print(matrix[i][j], end=" ")
